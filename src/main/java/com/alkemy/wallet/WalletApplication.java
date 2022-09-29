badfd18fe7d4e60c11d8239444d7fad5552f9a41
Yamil Amado Selene <amado.yamil@gmail.com>
2022-09-29 17:45:15 -0300

    adding project structure

---
+++ src/main/java/com/alkemy/wallet/WalletApplication.java
@@ -0,0 +1,15 @@
+package com.alkemy.wallet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.logging.Logger;
+
+@SpringBootApplication
+public class WalletApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WalletApplication.class, args);
+	}
+
+}
